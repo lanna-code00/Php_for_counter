2a23e173d2092ec9bda4f751ffa54d33c7751239
lanna-code00 <59274011+lanna-code00@users.noreply.github.com>
2020-07-28 04:02:02 -0700

    Update interface.js

--- interface.js
+++ interface.js
@@ -56,71 +56,70 @@ addEventListener('click', (event)=>{
 	console.log(fafa);
 });
 
-var item;
 
-var addItem = () =>{
-	//  $('#myModal').modal('hide');
-     all = [];
-	var c = Math.random();
-    var s = Math.floor(c*1000);
-	var v = 'ann'+ s;
-	var ey;
-    var ar = document.getElementsByClassName('allcounters');
-    for(var i = 0; i < ar.length;i++){
-        all.push(ar[i].value);
-    }
-    item = itemList.value;
 
-   defaltDiv.innerHTML += `<p><div class="input-group mb-3 rounded mt-5 col-md-6" id=`+v+s+`>
-		<table id=`+v+`Table>
-			<tr>
-		<td><div class="input-group-prepend">
-		<span class="input-group-text"
-		onmouseup="generalCount(`+s+`)" id="countNume" data-long-press-delay="2000" 
-		style="background: #0F0096 !important;min-height: 35px"><span class="${fafa} text-white">
-		</span></span>
-	  </div></td>
-	  <td><input type="text" class="form-control" value=`+item+` onmouseup="generalCount(`+s+`)"></td>
-	  <td><input type="number"  class="form-control p-3 text-white allcounters" value="0" readonly onmouseup="generalCount(`+s+`)"
-	  id=`+v+` data-long-press-delay="2000" style="background:#0F0096 !important"></td>
-	  <td><div class="input-group-append" id=`+v+`>
-		<span class="input-group-text" onclick="subInp(`+s+`)" style="min-height: 35px"><span class="fa fa-minus"></span></span>
-		<span class="input-group-text" style="min-height: 35px"><span class="fa fa-edit"></span></span>
-		<span class="input-group-text" style="min-height: 35px" onclick="del(`+s+`)"><span class="fa fa-times"></span></span>
-	  </div></td>
-	</tr>
-	</table>
-	</div>
-		</p>`;
-    document.getElementById('defaltDiv').style.display= "block";
-  
-         for(var i = 0; i < ar.length;i++){
-            ar[i].value = all[i];
-		} 
+
+function ui() {
+		//  $('#myModal').modal('hide');
+		var item = itemList.value;
+		all = [];
+		var c = Math.random();
+		var s = Math.floor(c*1000);
+		var v = 'ann'+ s;
+		var ar = document.getElementsByClassName('allcounters');
+		for(var i = 0; i < ar.length;i++){
+			all.push(ar[i].value);
+		}
+	  document.getElementById('defaltDiv').style.display= "block";
+	  
+			 for(var i = 0; i < ar.length;i++){
+				ar[i].value = all[i];
+			} 
+	defaltDiv.innerHTML += `<p><div class="input-group mb-3 rounded mt-5 col-md-6" id=`+v+s+`>
+	<table id=`+v+`Table>
+		<tr>
+	<td><div class="input-group-prepend">
+	<span class="input-group-text"
+	onmouseup="generalCount(`+s+`)" id="countNume" data-long-press-delay="2000" 
+	style="background: #0F0096 !important;min-height: 35px"><span class="${fafa} text-white">
+	</span></span>
+  </div></td>
+  <td><input type="text" class="form-control" value=`+item+` onmouseup="generalCount(`+s+`)"></td>
+  <td><input type="number"  class="form-control p-3 text-white allcounters" value="0" readonly onmouseup="generalCount(`+s+`)"
+  id=`+v+` data-long-press-delay="2000" style="background:#0F0096 !important"></td>
+  <td><div class="input-group-append" id=`+v+`>
+	<span class="input-group-text" onclick="subInp(`+s+`)" style="min-height: 35px"><span class="fa fa-minus"></span></span>
+	<span class="input-group-text" style="min-height: 35px"><span class="fa fa-edit"></span></span>
+	<span class="input-group-text" style="min-height: 35px" onclick="del(`+s+`)"><span class="fa fa-times"></span></span>
+  </div></td>
+</tr>
+</table>
+</div>
+	</p>`;
+}
+
+function addItem (){
+	var item = itemList.value;
 		if(item!="")
 		{
 			console.log('inserting...');
 			$.ajax({
 			method: "POST",
 			url: "http://localhost/Counter1/backendhome2.php",
-			data: {item, icons : fafa, noofitems:0},
+			data: {item: item, icons : fafa, noofitems:0},
             error: (xhr)=>{
 
 				console.log(`Error: ${xhr}`);
 			}, 
 			success: function(data, xhr) 
 			{
-				console.log(`Success: ${xhr}`);
-				/*if(res==1)
-				{
-				alert('Data saved successfully');	
+				if (NUmber(data) == 1) {
+					alert('Item already exists');
+					
+				} else {
+					alert('Item added');
+					ui();
 				}
-				else
-				{
-				alert('Data not saved');	
-				}
-				
-				*/
 			},
 		   
 			});
@@ -134,11 +133,15 @@ function clearall(){
 var bbt;
 
 var addInppt;
+
 function generalCount(c){
+	var item = itemList.value;
+	var post_id = $('#div_id').val();
 	console.log('updating...');
    var addInppt = 'ann' + c;
    console.log(addInppt);
    noOfItem = Number(document.getElementById(addInppt).value)+1;
+   alert(noOfItem);
    document.getElementById(addInppt).value =noOfItem ;
 
    $.ajax({
@@ -151,11 +154,16 @@ function generalCount(c){
 	}, 
 	success: function(data, xhr) 
 	{
-		console.log(`Success: ${xhr}`);
+		//console.log(`Success: ${xhr}`);
+		console.log(data);
 	},
+
+	
    
 	});
-}
+};
+
+
 let subInp = c => {
    var rt = 'ann' + c;
   console.log(rt);
